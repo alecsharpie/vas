76d8648524e017834247fd368940e7690e14a967
alec sharp <alecsharpie@gmail.com>
2024-02-24 19:50:21 +1100

    generate graph with nice formatting

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from datetime import date, timedelta
-import matplotlib.dates as mdates
-
+from matplotlib.dates import YearLocator, DateFormatter, AutoDateLocator, MonthLocator
 
 
 # Fetch historical data for VAS
 vas_data = yf.download(
     'VAS.AX', start='2003-01-01')  # Assuming VAS has data for 20 years
 vas_data['Date'] = vas_data.index
-
-print(vas_data.head())  # To verify the data
 
 vas_data['DateNumeric'] = vas_data['Date'].apply(lambda date: date.toordinal())
 
@@ -43,26 +40,24 @@ latest_date = vas_data['DateNumeric'].iloc[-1]
 latest_price = vas_data['Close'].iloc[-1]
 
 for timespan, (model, data) in models.items():
-    predicted_price = model.predict([[latest_date]])[0]
+    input_data = pd.DataFrame([[latest_date]], columns=['DateNumeric'])
+    predicted_price = model.predict(input_data)[0]
     print(
         f"{timespan} Model: Predicted Price = {predicted_price:.2f}, Actual Price = {latest_price:.2f}"
     )
 
-plt.figure(figsize=(20, 10))  # Increase the size of the figure
 fig, axs = plt.subplots(2, 2, figsize=(12, 8))  # Create 4 subplots
 
-fig.suptitle('VAS Price History with Linear Prediction', fontsize=20)
-
+fig.suptitle('VAS ETF', fontsize=16)
 
 # Flatten the axs array
 axs = axs.ravel()
 
 # Plot each model's prediction
-colors = ['red', 'blue', 'green',
-          'purple']  # Define a list of colors for the lines
+colors = ['red', 'blue', 'green','purple']  # Define a list of colors for the lines
 for i, (timespan, (model, data)) in enumerate(models.items()):
-    # Predict prices across the dataset's date range for the graph
-    predicted_prices = model.predict(data[['DateNumeric']])
+    input_data = pd.DataFrame(data[['DateNumeric']], columns=['DateNumeric'])
+    predicted_prices = model.predict(input_data)
     axs[i].plot(data['Date'],
                 predicted_prices,
                 label=f'{timespan} Prediction',
@@ -76,14 +71,22 @@ for i, (timespan, (model, data)) in enumerate(models.items()):
     axs[i].axhline(y=latest_price, color='gray',
                    linestyle='--')  # Add horizontal line for latest price
     axs[i].set_title(timespan, fontsize=15)  # Set title to just the timespan
-    axs[i].set_xlabel('Date', fontsize=8)  # Reduce font size
+    #axs[i].set_xlabel('Date', fontsize=8)  # Reduce font size
     axs[i].set_ylabel('Price (AUD)', fontsize=10)  # Reduce font size
-    axs[i].legend(fontsize=8)  # Reduce font size
+    axs[i].legend(fontsize=8, loc='upper left')  # Reduce font size
     axs[i].set_xlim([data['Date'].min(), data['Date'].max()])  # Set x-axis limits to the relevant data
 
     # Format x-axis
-    axs[i].xaxis.set_major_locator(mdates.AutoDateLocator())
-    axs[i].xaxis.set_major_formatter(mdates.DateFormatter('%y-%m-%d'))
+    if timespan in ['20Y', '3Y']:
+        axs[i].xaxis.set_major_locator(YearLocator())
+        axs[i].xaxis.set_major_formatter(DateFormatter('%Y'))
+    elif timespan in ['1Y']:
+        axs[i].xaxis.set_major_locator(MonthLocator())
+        axs[i].xaxis.set_major_formatter(DateFormatter('%y-%m'))
+    else:
+        axs[i].xaxis.set_major_locator(AutoDateLocator())
+        axs[i].xaxis.set_major_formatter(DateFormatter('%y-%m-%d'))
+
     plt.setp(axs[i].xaxis.get_majorticklabels(), rotation=45)
 
     # Limit y-axis
@@ -101,8 +104,8 @@ for i, (timespan, (model, data)) in enumerate(models.items()):
 
     # Buy/Sell annotation
     buy_sell_annotation = axs[i].text(
-        0.05,  # Adjust these values to move the text
-        0.15,
+        0.02,  # Adjust these values to move the text
+        0.1,
         f'{buy_sell_text}',
         transform=axs[i].transAxes,
         fontsize=12,  # Increase font size
@@ -112,8 +115,8 @@ for i, (timespan, (model, data)) in enumerate(models.items()):
 
     # Delta annotation
     delta_annotation = axs[i].text(
-        0.05,  # Adjust these values to move the text
-        0.10,  # Adjust this value to move the text
+        0.11,  # Adjust these values to move the text
+        0.1,  # Adjust this value to move the text
         f'Delta: ${delta:.2f}',
         transform=axs[i].transAxes,
         fontsize=10,
@@ -122,4 +125,5 @@ for i, (timespan, (model, data)) in enumerate(models.items()):
         bbox=dict(facecolor='white', alpha=0.7, edgecolor='black'))
 
 plt.tight_layout()  # Adjust layout for better spacing
+plt.savefig('VAS_results.png', dpi=300)  # Save the figure as 'figure.png' with a DPI of 300
 plt.show()
